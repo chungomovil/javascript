63645ea5db2688c1ec4d7379aa9e69226f10241b
chungomovil <yuaguapo@gmail.com>
2024-09-12 13:00:19 +0100

    subida 12-09-2024

--- ejercicios-john-mircha/ejercicios-dom/scripts/dom1.js
+++ ejercicios-john-mircha/ejercicios-dom/scripts/dom1.js
@@ -1,3 +1,4 @@
+/*ESTA FORMA ES SIN DELEGACION DE EVENTO (NO ES UNA BUENA PRACTICA)
 const $hamburguer = document.querySelector(".hamburger");
 const $nav = document.getElementById("navegacion");
 
@@ -11,4 +12,25 @@ function alternarNav(e) {
 $hamburguer.addEventListener("click", alternarNav);
 //No es necesario poner "children[0]", pero de este modo nos aseguramos que solo se desencadene cuando se de click al "ul"
 $nav.children[0].addEventListener("click", alternarNav);
+*/
+
+export default function hamburguerMenu (panelBtn, nav, navLink) {
+    const d = document;
+    const $panelBtn = d.querySelector(panelBtn);
+    const $nav = d.getElementById(nav);
+    
+    d.addEventListener("click", e => {
+        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) { /*Con la variable seguira del "*" seleccionamos todas las etiquetas hijas */
+            $panelBtn.classList.toggle("is-active");
+            ($nav.classList.contains("hiden"))
+            ? $nav.classList.replace("hiden", "normal")
+            : $nav.classList.replace("normal", "hiden"); 
+        }
+
+        if(e.target.matches(navLink)) {
+            $nav.classList.replace("normal", "hiden");
+        }
+    })
+
+} 
 
---
+++ ejercicios-john-mircha/ejercicios-dom/scripts/main.js
@@ -0,0 +1,5 @@
+import hamburguerMenu from "./dom1.js";
+
+window.addEventListener("DOMContentLoaded", e => {
+    hamburguerMenu(".hamburger", "navegacion", ".nav-item a");
+});
