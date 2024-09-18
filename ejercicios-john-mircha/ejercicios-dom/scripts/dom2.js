17b523d8452866ec1253a4174c67dd0b81bfbb77
chungomovil <yuaguapo@gmail.com>
2024-09-18 10:47:49 +0100

    subida 18-09-2024

--- ejercicios-john-mircha/ejercicios-dom/scripts/dom2.js
+++ ejercicios-john-mircha/ejercicios-dom/scripts/dom2.js
@@ -1,28 +1,55 @@
-export default function relojYAlarma(timer, clockBtn, clockBtnDisable) {
+export default function relojYAlarma(timer, clockBtn, clockBtnDisable, alarmBtn, alarmBtnDisable) {
     const d = document;
-    const $clockBtn = d.querySelector(clockBtn);
-    const $timer = d.querySelector(".timer");
+    const $timer = d.querySelector(timer);
     let clockId = "";
+    let alarmId = "";
 
     d.addEventListener("click", e => {
         if(e.target.matches(clockBtn)) {
             clockId = iniciarReloj(clockId, $timer);
-            $clockBtn.setAttribute("disabled","");
+            e.target.disabled = true;
         }
+
         if(e.target.matches(clockBtnDisable)) {
             $timer.textContent = "";
-            $clockBtn.removeAttribute("disabled");
+            d.querySelector(clockBtn).disabled = false;
             clearInterval(clockId);
             clockId = null;
+        }
+
+        if(e.target.matches(alarmBtn)) {
+            alarmId = iniciarAlarma();
+            e.target.disabled = true;
+        }
+
+        if(e.target.matches(alarmBtnDisable)) {
+            alarmId.muted = true;
+            d.querySelector(alarmBtn).disabled = false;
         }
     });
 }
 
-function iniciarReloj (clockId, timer) {
+function iniciarReloj(clockId, timer) {
     clockId = setInterval(() => {
+        /*let timeNow = new Date().toLocaleTimeString();*/ //Con este metodo es mas facil ya que viene la fecha codificada correctamente.
         let timeNow = new Date(Date.now());
-        let fullTime = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`;
+        const timeParts = [timeNow.getHours(), timeNow.getMinutes(), timeNow.getSeconds()];
+        const timeParts_mod = timeParts.map((el) => {
+            el = el.toString();
+            if(el.length < 2){
+                el = "0"+el;
+            }
+           return el;
+        });
+        let fullTime = `${timeParts_mod[0]}:${timeParts_mod[1]}:${timeParts_mod[2]}`;
         timer.textContent = fullTime;
     }, 1000);
     return clockId;
+}
+
+function iniciarAlarma(alarmId) {
+    alarmId = new Audio("./assets/clock-alarm.mp3");
+    alarmId.play();
+    alarmId.loop = true;
+    return alarmId;
 }
--- ejercicios-john-mircha/ejercicios-dom/scripts/main.js
+++ ejercicios-john-mircha/ejercicios-dom/scripts/main.js
@@ -3,5 +3,5 @@ import relojYAlarma from "./dom2.js";
 
 window.addEventListener("DOMContentLoaded", e => {
     hamburguerMenu(".hamburger", "navegacion", ".nav-item a");
-    relojYAlarma("timer", ".clock-btn", ".clock-btn-disable");
+    relojYAlarma(".timer", ".clock-btn", ".clock-btn-disable", ".alarm-btn", ".alarm-btn-disable");
 });
